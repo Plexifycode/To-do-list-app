e2bc59049e391869cf726706a4ebdb05297962da
Plexifycode <154735971+Plexifycode@users.noreply.github.com>
2024-01-30 21:50:55 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,34 +1,58 @@
 // selecting input and "add" buttn
 const enterTaskInpt = document.querySelector("#task-input");
 const addTaskBttn = document.querySelector("#add-task");
-
+const task = document.querySelectorAll(".task");
 const tasksContainer = document.querySelector(".tasks-container");
-console.log(tasksContainer, enterTaskInpt);
-
+const errorMsg = document.querySelector(".error");
 function Task() {
   this.name = enterTaskInpt.value;
   this.timeLine = null;
 }
 
-const taskArr = [];
+const toBeAdded = [];
+const tasksArr = [];
 
 addTaskBttn.addEventListener("click", () => {
-  const task = new Task();
-  taskArr.push(task);
+  if (!enterTaskInpt.value) {
+    errorMsg.classList.add("active");
+  } else {
+    errorMsg.classList.remove("active");
+    const task = new Task();
+    toBeAdded.push(task);
+    tasksArr.push(task);
 
-  enterTaskInpt.value = "";
+    enterTaskInpt.value = "";
 
-  for (let i = 0; i < taskArr.length; i++) {
-    const taskName = taskArr[i].name;
+    const taskName = toBeAdded[0].name;
 
     tasksContainer.innerHTML += `<div class="task"> 
-        <p>${taskName}</p>
+            <p>${taskName}</p>
+    
+            <button>
+                <span class="material-symbols-outlined"> delete </span>
+            </button>
+        </div>`;
 
-        <button>
-            <span class="material-symbols-outlined"> delete </span>
-        </button>
-    </div>`;
+    tasksContainer.style.display = "flex";
 
-    taskArr.splice(i, 1);
+    document.querySelectorAll(".task").forEach((task) => {
+      const taskName = task.querySelector("p");
+      taskName.addEventListener("click", () => {
+        taskName.classList.toggle("task-checked");
+      });
+
+      const deleteTaskBttn = task.querySelector("button");
+      deleteTaskBttn.addEventListener("click", () => {
+        task.remove();
+      });
+
+      if (tasksArr.length === 0) {
+        tasksContainer.style.display = "none";
+      } else {
+        tasksContainer.style.display = "flex";
+      }
+    });
+
+    tasksArr.splice(0, 1);
   }
 });
